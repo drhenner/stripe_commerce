502d72e06f83611bc985faf955f84642d6573fa2
David Henner <drhenner@yahoo.com>
2013-08-18 19:48:13 -0700

    add tests for preorders state and after preorders

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -9,6 +9,7 @@ describe ProductsController do
     @variant.stubs(:properties).returns(nil)
     @product.activate!
     stub_redirect_to_welcome
+    Settings.stubs(:allow_orders).returns(false)
   end
 
   it "index action should render index template" do
@@ -21,5 +22,27 @@ describe ProductsController do
     get :show, :id => @product.permalink
     #response.should render_template(:show)
     response.should redirect_to( preorders_url)
+  end
+end
+
+describe ProductsController do
+
+  before(:each) do
+    @product = create(:product)
+    @variant = create(:variant, :product => @product)
+    @variant.stubs(:primary_property).returns(nil)
+    @variant.stubs(:properties).returns(nil)
+    @product.activate!
+    stub_redirect_to_welcome
+  end
+
+  it "index action should render index template" do
+    get :index
+    response.should render_template(:index)
+  end
+
+  it "show action should not blow up without a property association" do
+    get :show, :id => @product.permalink
+    response.should render_template(:show)
   end
 end
