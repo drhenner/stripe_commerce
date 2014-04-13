6e27054d1f2e7400370688cb9b8c1263594b1304
David Henner <drhenner@yahoo.com>
2014-04-13 10:42:52 -0700

    bug in purchase orders

--- app/controllers/admin/inventory/purchase_orders_controller.rb
+++ app/controllers/admin/inventory/purchase_orders_controller.rb
@@ -60,7 +60,7 @@ class Admin::Inventory::PurchaseOrdersController < Admin::BaseController
   private
 
   def allowed_params
-    params.require(:purchase_order).permit(:supplier_id, :invoice_number, :tracking_number, :notes, :receive_po, :ordered_at, :estimated_arrival_on, :created_at, :updated_at, :total_cost)
+    params.require(:purchase_order).permit!
   end
 
   def form_info
